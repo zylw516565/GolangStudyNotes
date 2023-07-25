634cca887badca246e6485af52d9a93f68d10bac
root <root@localhost.localdomain>
2023-07-25 09:56:18 +0800

    end section.3.5.5

--- ch3/section.3.5.5.go
+++ ch3/section.3.5.5.go
@@ -10,4 +10,13 @@ func main() {
 	y := fmt.Sprintf("%d", s)
 	fmt.Println(y)
 	fmt.Println(strconv.Itoa(s))
+	fmt.Println(strconv.FormatInt(int64(s), 2))
+
+	m := fmt.Sprintf("x=%b", s)
+	fmt.Println(m)
+
+	a, _ := strconv.Atoi("123")
+	b, _ := strconv.ParseInt("123", 10, 64)
+	fmt.Println(a, b)
+	fmt.Printf("a type = %T, b type = %T\n", a, b)
 }
