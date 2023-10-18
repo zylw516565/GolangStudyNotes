23ed932a9a1f6fdf612d52f3955387511c53b2e7
张营磊 <zhyinglei@szlanyou.com>
2023-10-18 16:22:47 +0800

    add section12.1 section12.2 section12.3

---
+++ ch12/section12.1.go
@@ -0,0 +1,27 @@
+package main
+
+import "strconv"
+
+func Sprint(x interface{}) string {
+	type stringer interface {
+		String() string
+	}
+
+	switch x := x.(type) {
+	case stringer:
+		return x.String()
+	case string:
+		return x
+	case int:
+		return strconv.Itoa(x)
+	// ...similar cases for int16, uint32, and so on...
+	case bool:
+		if x {
+			return "true"
+		}
+		return "false"
+	default:
+		// array, chan, func, map, pointer, slice, struct
+		return "???"
+	}
+}
